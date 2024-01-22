dcc8b539a41ec556016c1283a38de4073b40633c
Nigel Nindo <nigelnindodev@gmail.com>
2024-01-22 15:07:01 +0300

    add elysia html

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(html())
+  .get("/html", () => {
+    return `<html lang="en">
+            <head>
+                <title>Hello World</title>
+            </head>
+            <body>
+                <h1>Hello World</h1>
+            </body>
+        </html>`;
+  })
+  .get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
