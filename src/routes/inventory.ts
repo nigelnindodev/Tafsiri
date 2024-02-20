c505b7afe053071d5cced2fe9b40f9e1e6c2a9fc
Nigel Nindo <nigelnindodev@gmail.com>
2024-02-20 20:21:50 +0300

    Start adding Zod validationw

--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -1,9 +1,21 @@
-import Elysia from "elysia"
+import Elysia from "elysia";
+import { z } from "zod";
+
 import { CreateInventorySection } from "../components/pages/inventory/create";
 import { ViewInventorySection } from "../components/pages/inventory/inventory";
 import { DataSource } from "typeorm";
 import { createInventoryItem, listInventoryItemOrders, listInventoryItems, searchInventoryItems } from "../services/inventory";
 import { InventoryPage } from "../components/pages/inventory";
+
+const inventorySchemas = {
+  seacrhInventoryItemsQuery: z.object({
+    search: z.string()
+  }),
+  createInventoryItemBody: z.object({
+    name: z.string(),
+    price: z.number()
+  })
+};
 
 export const inventoryRoutes = (dataSource: DataSource) => {
   const app = new Elysia({ prefix: "/inventory" });
@@ -15,18 +27,20 @@ export const inventoryRoutes = (dataSource: DataSource) => {
       return await listInventoryItems(dataSource)
     })
     .get("/list/search", async (ctx) => {
-      const searchTerm = ctx.query.search;
-      if (searchTerm === "") {
+      const validateResult = inventorySchemas.seacrhInventoryItemsQuery.parse(ctx.query);
+      if (validateResult.search === "") {
         return await listInventoryItems(dataSource);
       } else {
-        return await searchInventoryItems(dataSource, searchTerm);
+        console.log("Schema result:", validateResult);
+        return await searchInventoryItems(dataSource, validateResult.search);
       }
     })
     .get("/orders/:inventoryId", async (ctx) => {
       return await listInventoryItemOrders(dataSource, Number(ctx.params.inventoryId));
     })
     .post("/create", async (ctx) => {
-      return await createInventoryItem(dataSource, ctx.body.name, Number(ctx.body.price));
+      const validateResult = inventorySchemas.createInventoryItemBody.parse(ctx.body);
+      return await createInventoryItem(dataSource, validateResult.name, validateResult.price);
     });
   return app;
 } 
