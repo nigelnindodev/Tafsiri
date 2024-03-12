41b6f385db26de45cefb1cad26e900b581446458
Nigel Nindo <nigelnindodev@gmail.com>
2024-03-12 09:11:30 +0300

    Fix initial error with tests starting much faster than postgres initialization

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,14 @@
+import "reflect-metadata"; // required for TypeORM
+
 import { describe, expect, it } from "bun:test";
 
 import { createApplicationServer } from "../src/server.ts";
 import { PostgresDataSourceSingleton } from "../src/postgres/index.ts";
 
 describe("Server Testing", async () => {
-  const dataSource = await PostgresDataSourceSingleton.getInstance();
   it("Correcly loads up the test server", async () => {
+    const dataSource = await PostgresDataSourceSingleton.getInstance();
+    console.log("Fetched postgres datasource");
     const app = createApplicationServer(dataSource);
     const response = await app
       .handle(new Request("http://localhost:3000"))
--- test_script.ts
+++ test_script.ts
@@ -1,7 +1,12 @@
 import { $ } from "bun";
+import { setTimeout } from "timers/promises";
 
 // Use docker compose to set up a postgres database as specified in compose.yaml
 await $`docker compose up -d`;
+
+// We might be running tests much faster than when our postgres container is ready to handle
+// them. Set up a 5 second delay before starting testing.
+await setTimeout(5000);
 
 // Run tests
 await $`bun run test`;
