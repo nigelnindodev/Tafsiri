ca3ad9b094a5b664ef143b5cf1cdc52b2092ffd7
Nigel Nindo <nigelnindodev@gmail.com>
2024-03-12 10:32:07 +0300

    Setup deliberately failing test

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,14 +6,17 @@ import { createApplicationServer } from "../src/server.ts";
 import { PostgresDataSourceSingleton } from "../src/postgres/index.ts";
 
 describe("Server Testing", async () => {
+  console.log("NODE_ENV: ", process.env.NODE_ENV);
+  const dataSource = await PostgresDataSourceSingleton.getInstance();
+  console.log("Fetched postgres datasource");
+  const app = createApplicationServer(dataSource);
   it("Correcly loads up the test server", async () => {
-    console.log("NODE_ENV: ", process.env.NODE_ENV);
-    const dataSource = await PostgresDataSourceSingleton.getInstance();
-    console.log("Fetched postgres datasource");
-    const app = createApplicationServer(dataSource);
     const response = await app
       .handle(new Request("http://localhost:3000"))
       .then((res) => res.status);
     expect(response).toBe(200);
+  });
+  it("Fails", async () => {
+    expect(1).toBe(2);
   });
 });
