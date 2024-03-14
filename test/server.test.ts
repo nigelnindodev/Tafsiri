ddb8384a973f24321caf8704080f18707cc4a352
Nigel Nindo <nigelnindodev@gmail.com>
2024-03-14 12:01:39 +0300

    Added root endpoint tests

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata"; // required for TypeORM
+import * as cheerio from "cheerio";
 
 import { describe, expect, test } from "bun:test";
 
 import { createApplicationServer } from "../src/server.ts";
 import { PostgresDataSourceSingleton } from "../src/postgres/index.ts";
+import { ServerHxTriggerEvents } from "../src/services/common/constants.ts";
+import { HtmxTargets } from "../src/components/common/constants.ts";
 
 describe("Root server", async () => {
   describe("Root / endpoint GET API call", async () => {
@@ -19,6 +22,23 @@ describe("Root server", async () => {
       expect(response.headers.get("content-type")).toInclude("text/html");
     });
 
-    test("Response htmx should include markup that loads the root component", () => {});
+    test("Response htmx should include markup that loads the root component when the content is loaded", async () => {
+      const $ = cheerio.load(await response.text());
+      const elementsWithHxGet = $("div[hx-get]");
+
+      elementsWithHxGet.each((index, element) => {
+        if (index === 0) {
+          const hxGetValue = $(element).attr("hx-get");
+          expect(hxGetValue).toBe("/root");
+
+          const hxTriggerValue = $(element).attr("hx-trigger");
+          expect(hxTriggerValue).toInclude("load");
+          expect(hxTriggerValue).toInclude(ServerHxTriggerEvents.LOGIN_STATUS_CHANGE);
+
+          const hxTargetValue = $(element).attr("hx-target");
+          expect(hxTargetValue).toInclude(HtmxTargets.ROOT_DIV);
+        }
+      });
+    });
   });
 });
